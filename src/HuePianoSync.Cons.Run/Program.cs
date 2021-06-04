7340f626f3fe1d10ea07392a52629829892711bf
Michal Ogluszka <michal.ogluszka@outlook.com>
2021-06-04 21:21:43 +0200

    hue brdige discovered

---
+++ src/HuePianoSync.Cons.Run/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HuePianoSync.Cons.Run
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Hue!");
+
+            HueSync sync = new HueSync();
+
+            var initTask = sync.Initialize();
+            var init = await initTask;            
+        }
+    }
+}
---
+++ src/HuePianoSync/HueSync.cs
@@ -0,0 +1,21 @@
+﻿using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using Q42.HueApi.Models.Bridge;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HuePianoSync
+{
+    public class HueSync
+    {    
+        public async Task<IEnumerable<LocatedBridge>> Initialize()
+        {
+            IBridgeLocator locator = new HttpBridgeLocator(); //Or: LocalNetworkScanBridgeLocator, MdnsBridgeLocator, MUdpBasedBridgeLocator
+            var bridges = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            return bridges;
+
+            
+        }
+    }
+}
